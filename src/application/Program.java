09bf90a198eb3dc785514070a19518f48bf5da1b
Kaique <kaique.andruid@gmail.com>
2024-05-08 15:47:27 -0300

    findById implementation and Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,14 +9,10 @@ import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1,"Books");
-        System.out.println(obj);
-
-        Seller seller = new Seller(1,"Maria Souza","mariasouza@gmail.com",new Date(),3000.0,obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("=== TEST 1: seller findById =====");
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
-
     }
 }
